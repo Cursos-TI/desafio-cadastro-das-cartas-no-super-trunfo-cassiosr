92c91e48844871f50fecc9ac81a6e36865c3179d
cassiosr <202307044407@alunos.unifavip.edu.br>
2025-07-31 01:16:57 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,22 +8,21 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    char estado, codigo, cidade;
-    int populacao, pontos;
+    char cidade;
+    int estado, populacao, pontos, codigo;
     float area, PIB;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("Digite o codigo do estado de Carta 1 de A a H: \n");
-    scanf("%s", &estado);
+    scanf("%d", &estado);
     printf("Digite o codigo da carta: \n");
-    scanf("%s", &codigo);
+    scanf("%d", &codigo);
     printf("Digite a cidade: \n");
-    fget(cidade, 50, stdin);
-    cidade[strcspn(cidade, "\n")]=0;
-    prinf("Digite a população: \n");
-    scanf("%f", &populacao);
+    scanf("%s", &cidade);
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
     printf("Digite a área da cidade: \n");
     scanf("%f", &area);
     printf("Digite o PIB: \n");
@@ -34,7 +33,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("Carta1\n" "Estado: %.2s\n" "Código: %s\n" "Nome da cidade: %s\n" "População: %f\n" "Área: %.2f\n" "PIB: %f\n" "Número de Pontos Turísticos: %d\n", estado, codigo, cidade, populacao, area, PIB);
+    printf("Carta1\n Estado: %d\n Código: %d\n Nome da cidade: %s\n População: %d\n Área: %f\n PIB: %f\n Número de Pontos Turísticos: %d\n", estado, codigo, cidade, populacao, area, PIB);
 
     return 0;
 }
